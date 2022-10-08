ea56d8d1b2f1e96916a9d22ae0b1467044d2b148
Desire01 <yavlumene@gmail.com>
2022-10-08 09:32:35 +0200

    More changes

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -34,11 +34,11 @@ export const IncomeExpenses = () => {
   return (
     <div className="inc-exp-container">
         <div>
-          <h4>Income</h4>
+          <h4>Personal Income</h4>
   <p className="money plus">{moneyFormatter(income)}</p>
         </div>
         <div>
-          <h4>Expense</h4>
+          <h4>Personal Expense</h4>
   <p className="money minus">{moneyFormatter(expense)}</p>
         </div>
       </div>
